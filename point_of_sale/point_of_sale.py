fc3716d5f4609b7645d17bde7cf3a83d443d305c
agb80 <atin81@gmail.com>
2013-03-05 16:29:35 -0600

    Added payment logic on create_from_ui function

--- point_of_sale/point_of_sale.py
+++ point_of_sale/point_of_sale.py
@@ -36,13 +36,6 @@ import openerp.addons.product.product
 
 _logger = logging.getLogger(__name__)
 
-class pos_prueba(osv.osv):
-    _name="pos.prueba"
-    def test(self, cr, uid, ids, context=None):
-        print 'hola'               
-        return True
-pos_prueba()    
-
 class pos_config(osv.osv):
     _name = 'pos.config'
 
@@ -513,11 +506,36 @@ class pos_order(osv.osv):
                     'pos_reference':order['name'],
                     'partner_id':partner_id,
                 }, context)          
+                
+            for payments in order['statement_ids']:
+                payment = payments[2]
+                self.add_payment(cr, uid, order_id, {
+                    'amount': payment['amount'] or 0.0,
+                    'payment_date': payment['name'],
+                    'statement_id': payment['statement_id'],
+                    'payment_name': payment.get('note', False),
+                    'journal': payment['journal_id']
+                }, context=context)
+
+            if order['amount_return'] > 0:
+                session = self.pool.get('pos.session').browse(cr, uid, order['pos_session_id'], context=context)
+                cash_journal = session.cash_journal_id
+                cash_statement = False
+                if not cash_journal:
+                    cash_journal_ids = filter(lambda st: st.journal_id.type=='cash', session.statement_ids)
+                    if not len(cash_journal_ids):
+                        raise osv.except_osv( _('error!'),
+                            _("No cash statement found for this session. Unable to record returned cash."))
+                    cash_journal = cash_journal_ids[0].journal_id
+                self.add_payment(cr, uid, order_id, {
+                    'amount': -order['amount_return'],
+                    'payment_date': time.strftime('%Y-%m-%d %H:%M:%S'),
+                    'payment_name': _('return'),
+                    'journal': cash_journal.id,
+                }, context=context)
             order_ids.append(order_id)
-            wf_service = netsvc.LocalService("workflow")
-            wf_service.trg_validate(uid, 'pos.order', order_id, 'draft', cr)
+            self.signal_paid(cr, uid, [order_id])
         return order_ids
-    #*********************************************************************************
 
     def unlink(self, cr, uid, ids, context=None):
         for rec in self.browse(cr, uid, ids, context=context):
