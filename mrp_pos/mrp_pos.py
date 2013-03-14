c7e1f1ca783ca3f5bac688927571406167ee4fa5
Gerardogg <proyectosopen@gmail.com>
2013-03-13 19:53:59 -0600

    Bug arreglado en punto de venta funcional con produccion en estado esperando materiales
    Conflicts:
    	point_of_sale/point_of_sale.py

--- mrp_pos/mrp_pos.py
+++ mrp_pos/mrp_pos.py
@@ -1,15 +1,9 @@
-import time
-from datetime import datetime
-
 import openerp.addons.decimal_precision as dp
 from openerp.osv import fields, osv
-from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT, DATETIME_FORMATS_MAP
 from openerp.tools import float_compare
 from openerp.tools.translate import _
 from openerp import netsvc
 from openerp import tools
-
-
 
 class pos_order(osv.osv):
     """
@@ -18,77 +12,38 @@ class pos_order(osv.osv):
     _name = 'pos.order'
     _inherit = 'pos.order'
     
-    _description = 'Extencion de orden de produccion'
+    def create_order(self, cr, uid, order, partner_id, context = None):        
+        order_id = super(pos_order, self).create_order(cr, uid, order, partner_id, context)
+        self._produces(cr, uid, self.pool.get('pos.order').browse(cr, uid, order_id, context), context)        
+        return order_id  
     
-    def test(self, cr, uid, ids, context=None):
-        pos_order = self.browse(cr, uid, ids, context=None)[0]                
-        for line in pos_order.lines:
-            
-            bom_id = self.pool.get('mrp.bom')._bom_find(cr,uid,line.product_id.id,line.product_id.uom_id.id)
-            if bom_id:   
-                """mrp_obj=self.pool.get('mrp.production').create(cr, uid, {
-                               'product_id':"3",                                                        
-                               'bom_id':"1",
-                               'product_uom':"1",
-                               'product_qty':"1",
-                               },context=None)"""                       
-        return True
+    def update_order(self, cr, uid, order, partner_id, context = None):        
+        order_id = super(pos_order, self).update_order(cr, uid, order, partner_id, context)
+        self._produces(cr, uid, self.pool.get('pos.order').browse(cr, uid, order_id, context), context)        
+        return order_id
     
-    def produces(self, cr, uid, order, context=None):    
-        
-        ids=order['id'] 
-                 
-        
+    def _produces(self, cr, uid, order, context = None):    
         for line in order.lines:
-            bom_id=self.pool.get('mrp.bom')._bom_find(cr,uid,line.product_id.id,line.product_id.uom_id.id)            
+            bom_id = self.pool.get('mrp.bom')._bom_find(cr,uid,line.product_id.id,line.product_id.uom_id.id)            
             if bom_id:
-                if line.prod_qty < line.qty:                    
-                    
-                    mrp_obj=self.pool.get('mrp.production').create(cr, uid, {
+                if line.produced < line.qty:                    
+                    mrp_obj = self.pool.get('mrp.production').create(cr, uid, {
                                    'product_id':line.product_id.id,                                                        
                                    'bom_id':bom_id,
                                    'product_uom':line.product_id.uom_id.id,
-                                   'product_qty':(line.qty)-line.prod_qty
-                                                                       },context=None)                   
-                    #actuliza prod_qty y realiza picking
-                    self.pool.get('pos.order.line').write(cr, uid, line.id, { 'prod_qty':line.qty}, context)                    
-                    temp= self.pool.get('mrp.production').browse(cr,uid,mrp_obj)
-                    print temp._id
+                                   'product_qty':line.qty - line.produced
+                               },context=None)                   
+                    #actuliza produce y realiza picking
+                    self.pool.get('pos.order.line').write(cr, uid, line.id, {'produced': line.produced + (line.qty - line.produced)}, context)                    
+                    temp = self.pool.get('mrp.production').browse(cr,uid,mrp_obj)
                     self.pool.get('mrp.production').action_confirm(cr, uid, [temp.id], context)                    
                 else:                    
-                    if line.prod_qty > line.qty:
-                        return false                                                                   
+                    if line.produced > line.qty:
+                        #TODO: Check if is possible to reduce the quantity
+                        return false                                           
         return True    
+        
     
-    def update_lines(self, cr, uid, order_id, lines = None, context = None):        
-        if lines !=None:
-            #actulizacion            
-            super(pos_order, self).update_lines(cr, uid, order_id[0], lines, context)
-            orders=self.pool.get('pos.order').browse(cr, uid, order_id, context)[0]
-            print 'Estado de producion',self.produces(cr, uid, orders, context=None)
-        else:
-            #creacion
-            orders=self.pool.get('pos.order').browse(cr, uid, order_id, context)
-            print 'Estado de producion',self.produces(cr, uid, orders, context=None)
-        return True  
-    
-    def get_current_lines(self,order_lines):
-    #calcula ordenes actuales        
-        current_lines=[]
-        for line1 in order_lines:
-            d= {                            
-                        'discount':line1.discount,
-                        'price_unit':line1.price_unit,
-                        'product_id':line1.product_id.id,
-                        'qty':line1.qty,                        
-                        'prod_qty':line1.qty,
-                        'id':line1.id,                         
-                    }
-            current_lines.append(d)
-        return current_lines
-    
-    
-
 pos_order()
 #********************************************************************cambio******************************************************** 
 class pos_order_line(osv.osv):
@@ -97,10 +52,10 @@ class pos_order_line(osv.osv):
     _inherit = 'pos.order.line'
     
     _columns = {
-                'prod_qty': fields.integer('Bquantity', digits=(16, 2)),                
+                'produced': fields.integer('produced', digits=(16, 2)),                
                 }
     _defaults = {
-                 'prod_qty': lambda *a: 0,                 
+                'produced': lambda *a: 0,                 
                  }                
 pos_order_line()
 
--- point_of_sale/point_of_sale.py
+++ point_of_sale/point_of_sale.py
@@ -486,9 +486,9 @@ class pos_order(osv.osv):
             order = tmp_order['data']
             partner_id = order['partner_id'] if 'partner_id' in order else False
             if 'order_id' in order: 
-                order_id = self.update_sales(cr, uid, order, partner_id, context)
+                order_id = self.update_order(cr, uid, order, partner_id, context)
             else: 
-                order_id = self.create_sales(cr, uid, order, partner_id, context)
+                order_id = self.create_order(cr, uid, order, partner_id, context)
                 
             for payments in order['statement_ids']:
                 payment = payments[2]
@@ -516,25 +516,15 @@ class pos_order(osv.osv):
                     'payment_name': _('return'),
                     'journal': cash_journal.id,
                 }, context=context)
-            order_ids.append(order_id)
-            """try:
-             order_idf = order_id[0]
-            except TypeError, te:
-                order_id=[order_id]"""             
+            order_ids.append(order_id)          
             self.signal_paid(cr, uid, order_ids)
         return order_ids
     
-    def update_sales(self, cr, uid, order, partner_id, context = None):
+    def update_order(self, cr, uid, order, partner_id, context = None):
         order_id = order['order_id']
-        
-        """try: 
-            if order_id[0] != []:
-                order_id=order_id[0]                
-        except TypeError, te:
-            print 'Dato: ',order_id"""
             
         if order_id != []:
-            actualiza=self.write(cr, uid, order_id, {
+            self.write(cr, uid, order_id, {
                            'user_id': order['user_id'] or False,
                            'session_id': order['pos_session_id'],                    
                            'pos_reference':order['name'],
@@ -547,7 +537,7 @@ class pos_order(osv.osv):
                 return False
         return order_id
     
-    def create_sales(self, cr, uid, order, partner_id, context = None):
+    def create_order(self, cr, uid, order, partner_id, context = None):
         order_id = self.create(cr, uid, {
                     'name': order['name'],
                     'user_id': order['user_id'] or False,
@@ -556,13 +546,12 @@ class pos_order(osv.osv):
                     'pos_reference':order['name'],
                     'partner_id':partner_id,                    
                 }, context)          
-        #temp = self.pool.get('pos.order').browse(cr, uid, order_id)        
-        #self.update_lines(cr, uid, temp.id, context)
         return order_id
     
     def get_dic(self,seq, key):
         return dict((d[key], dict(d, index=index)) for (index, d) in enumerate(seq))
     
+<<<<<<< HEAD
     def get_current_lines(self, cr, uid, order_id, context):
         
         """try:
@@ -572,18 +561,20 @@ class pos_order(osv.osv):
                 print 'Dato: ',order_id """
         
         order_lines= self.pool.get('pos.order').browse(cr, uid, order_id, context).lines        
+=======
+    def get_current_lines(self, cr, uid, order_id, context = None):        
+        order_lines = self.pool.get('pos.order').browse(cr, uid, order_id, context).lines        
+>>>>>>> 5eedbba... Bug arreglado en punto de venta funcional con produccion en estado esperando materiales
         #calcula ordenes actuales              
-        current_lines=[]
+        current_lines = []
         for line in order_lines:
-            print line,
-            d= {                            
-                        'discount':line.discount,
-                        'price_unit':line.price_unit,
-                        'product_id':line.product_id.id,
-                        'qty':line.qty,
-                        'id':line.id,                         
-                    }
+            d= {'discount':line.discount,
+                'price_unit':line.price_unit,
+                'product_id':line.product_id.id,
+                'qty':line.qty,
+                'id':line.id}
             current_lines.append(d)
+            
         return current_lines
         
     def update_lines(self, cr, uid, order_id, lines = None, context = None):
@@ -592,17 +583,14 @@ class pos_order(osv.osv):
         """
         #TODO optimizar actualizar,añador y remover lineas de productos
         #ordenes nuevas
-        if lines !=None:
+        if lines:
             new_lines = []
             for line in lines:                                    
                 new_lines.append(line[2])        
             dic_new_lines = self.get_dic(new_lines,'product_id')
             
-                        
-            current_lines=self.get_current_lines(cr, uid, order_id, context )            
+            current_lines = self.get_current_lines(cr, uid, order_id, context)            
             dic_current = self.get_dic(current_lines,'product_id')
-            
-            print "Dicionario de diccionarios: ",dic_current
             
             #conjuntos eliminacion de ordenes que ya no estan en las nuevas ordes        
             x = set(dic_new_lines)       
@@ -610,30 +598,21 @@ class pos_order(osv.osv):
             z = y-x
             for element in z:            
                 a = dic_current[element]
-                print "Elemento a eliminar: ",a
                 self.pool.get('pos.order.line').unlink(cr, uid,a['id'],context)
           
-            """try:
-                if order_id[0] != []:
-                    order_id=order_id[0]                
-            except TypeError, te:
-                print 'Dato: ',order_id"""
-          
             for lin in new_lines:
-               print "Orden actual : ",current_lines
-               print "Intentando grabar producto : ",lin               
                if lin['product_id']  not in  dic_current:
-                  lin['order_id'] = order_id              
-                  print "Agregado linea de producto: ",lin
+                  lin['order_id'] = order_id
                   self.pool.get('pos.order.line').create(cr, uid, lin, context)
-               else:
-                   print "Registro actulizado en: linea de producto: ",lin
+               else: 
                    self.pool.get('pos.order.line').write(cr, uid,dic_current[lin['product_id']]['id'], {                               
                               'discount':lin['discount'],
                               'price_unit':lin['price_unit'],
                               'qty':lin['qty'],
                           }, context)
-        else: return False      
+        else: 
+            return False      
+        
         return True   
 
     def unlink(self, cr, uid, ids, context=None):
