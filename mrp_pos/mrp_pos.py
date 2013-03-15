0ae8fd8c3b9723a7a746e1672cb593cee48aa4d0
Gerardogg <proyectosopen@gmail.com>
2013-03-15 15:19:10 -0600

    Pos with mrp production funcional #2
    was repalce  old function to send a signal to change estare prom production on ready to produce

--- mrp_pos/mrp_pos.py
+++ mrp_pos/mrp_pos.py
@@ -1,9 +1,4 @@
-import openerp.addons.decimal_precision as dp
 from openerp.osv import fields, osv
-
-from openerp.tools import float_compare
-from openerp.tools.translate import _
-from openerp import netsvc
 from openerp import tools
 
 class pos_order(osv.osv):
@@ -23,29 +18,28 @@ class pos_order(osv.osv):
         self._produces(cr, uid, self.pool.get('pos.order').browse(cr, uid, order_id, context), context)        
         return order_id
     
-    def _produces(self, cr, uid, order, context = None):    
+    def _produces(self, cr, uid, order, context = None):
+        production_obj = self.pool.get('mrp.production')    
         for line in order.lines:
-            bom_id = self.pool.get('mrp.bom')._bom_find(cr,uid,line.product_id.id,line.product_id.uom_id.id)            
-            if bom_id:
-                if line.produced < line.qty:                    
-                    mrp_obj = self.pool.get('mrp.production').create(cr, uid, {
-                                   'product_id':line.product_id.id,                                                        
-                                   'bom_id':bom_id,
-                                   'product_uom':line.product_id.uom_id.id,
-                                   'product_qty':line.qty - line.produced
-                               },context=None)                   
-                    #actuliza produce y realiza picking
-                    self.pool.get('pos.order.line').write(cr, uid, line.id, {'produced': line.produced + (line.qty - line.produced)}, context)                    
-                    temp = self.pool.get('mrp.production').browse(cr,uid,mrp_obj)
-                    self.pool.get('mrp.production').action_confirm(cr, uid, [temp.id], context)                    
-                else:                    
-                    if line.produced > line.qty:
-                        #TODO: Check if is possible to reduce the quantity
-                        return false                                           
+            bom_id = self.pool.get('mrp.bom')._bom_find(cr, uid, line.product_id.id, line.product_id.uom_id.id)            
+            if bom_id and (line.produced < line.qty):                    
+                mrp_id = production_obj.create(cr, uid, {
+                               'product_id':line.product_id.id,                                                        
+                               'bom_id':bom_id,
+                               'product_uom':line.product_id.uom_id.id,
+                               'product_qty':(line.qty - line.produced)
+                           }, context = None)                   
+                #actuliza produce y realiza picking
+                self.pool.get('pos.order.line').write(cr, uid, line.id, {'produced': line.produced + (line.qty - line.produced)}, context)                
+                production_obj.signal_button_confirm(cr, uid, [mrp_id])
+                
+            if bom_id and (line.produced > line.qty):
+                #TODO: Check if is possible to reduce the quantity
+                return false                                           
         return True    
 
 pos_order()
-#********************************************************************cambio******************************************************** 
+
 class pos_order_line(osv.osv):
     
     _name = 'pos.order.line'
@@ -54,6 +48,7 @@ class pos_order_line(osv.osv):
     _columns = {
                 'produced': fields.integer('produced', digits=(16, 2)),                
                 }
+    
     _defaults = {
                 'produced': lambda *a: 0,                 
                  }                
